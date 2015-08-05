a0fec01a62a10c98637b7aadac7b3c341ad30980
Dong Dung <tiendungdong@gmail.com>
2015-08-05 03:29:01 +0000

    init commit

---
+++ application/controllers/Install.php
@@ -0,0 +1,25 @@
+<?php
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*	
+ *	@author 	: Joyonto Roy
+ *	date		: 27 september, 2014
+ *	Ekattor School Management System Pro
+ *	http://codecanyon.net/user/Creativeitem
+ *	support@creativeitem.com
+ */
+
+class Install extends CI_Controller
+{
+    
+    
+    /***default functin, redirects to login page if no admin logged in yet***/
+    public function index()
+    {
+        $this->load->view('backend/install');
+    }
+    
+    
+    
+}
---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2014 <strong>Ekattor School Manager</strong>. 
+    Developed by 
+	<a href="http://creativeitem.com" 
+    	target="_blank">Creativeitem</a>
+</footer>
